31cc09d544843ffea553b347279c4c766d29eaa2
Nathan Sears <natsears56@gmail.com>
2016-06-15 15:18:45 -0400

    Delete Predator.java

--- src/Predator.java
+++
@@ -1,1 +0,0 @@
-import java.awt.*;import java.util.*;public interface Predator{	/**	*	Method returns true if obj is a type the animal can eat,	*	returns false otherwise	*	@param	obj	object to be evaluated	*	@return true if obj can be eaten, false otherwise	*/	public boolean isSomethingICanEat(Animal obj);}
